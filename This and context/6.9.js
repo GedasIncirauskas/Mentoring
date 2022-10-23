ee53361a3850a395c32cf0b1f344cfa9260ee49d
GedasIncirauskas <gedas.inc@gmail.com>
2022-10-23 11:51:36 +0300

    6.9 not finished last tast

--- This and context/6.9.js
+++ This and context/6.9.js
@@ -57,3 +57,83 @@ f1500("test"); // shows "test" after 1500ms
 // but your solution should pass all arguments and the context this.
 
 // _________________________________________________________________________________________
+
+// The result of debounce(f, ms) decorator is a wrapper that suspends calls to f
+// until there’s ms milliseconds of inactivity (no calls, “cooldown period”),
+// then invokes f once with the latest arguments.
+
+// In other words, debounce is like a secretary that accepts “phone calls”,
+// and waits until there’s ms milliseconds of being quiet.
+// And only then it transfers the latest call information to “the boss” (calls the actual f).
+
+// For instance, we had a function f and replaced it with f = debounce(f, 1000).
+
+// Then if the wrapped function is called at 0ms, 200ms and 500ms,
+// and then there are no calls, then the actual f will be only called once, at 1500ms.
+// That is: after the cooldown period of 1000ms from the last call.
+
+function debounce(func, ms) {
+  let timeout;
+  return function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, arguments), ms);
+  };
+}
+
+let f = debounce(alert, 1000);
+
+f("a");
+setTimeout(() => f("b"), 200);
+setTimeout(() => f("c"), 500);
+// debounced function waits 1000ms after the last call and then runs: alert("c")
+
+// Call debounce function (func, ms), it returns a wrapper.
+// When called, it schedules the original function call after given ms and cancel previous timeout.
+
+// _________________________________________________________________________________________
+
+// Create a “throttling” decorator throttle(f, ms) – that returns a wrapper.
+// Visually, it will look like this:
+
+// For the first mouse movement the decorated variant immediately passes the call to update.
+// That’s important, the user sees our reaction to their move immediately.
+// Then as the mouse moves on, until 100ms nothing happens. The decorated variant ignores calls.
+// At the end of 100ms – one more update happens with the last coordinates.
+// Then, finally, the mouse stops somewhere. The decorated variant waits until 100ms expire
+// and then runs update with last coordinates. So, quite important, the final mouse coordinates are processed.
+// A code example:
+
+// function f(a) {
+//   console.log(a);
+// }
+
+// // f1000 passes calls to f at maximum once per 1000 ms
+// let f1000 = throttle(f, 1000);
+
+// f1000(1); // shows 1
+// f1000(2); // (throttling, 1000ms not out yet)
+// f1000(3); // (throttling, 1000ms not out yet)
+
+// // when 1000 ms time out...
+// // ...outputs 3, intermediate value 2 was ignored
+
+function throttle(func, ms) {
+  return function () {
+    func.apply(this, arguments);
+  };
+}
+
+function f(a) {
+  console.log(a);
+}
+
+// f1000 passes calls to f at maximum once per 1000 ms
+let f10001 = throttle(f, 1000);
+
+f10001(1); // shows 1
+f10001(2); // (throttling, 1000ms not out yet)
+f10001(3); // (throttling, 1000ms not out yet)
+
+// when 1000 ms time out...
+// ...outputs 3, intermediate value 2 was ignored
+// NEED HELP HERE
