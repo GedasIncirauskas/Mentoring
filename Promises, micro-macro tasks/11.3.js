e8cd41757d61b6f86e15c2b4546f834357f32d79
GedasIncirauskas <gedas.inc@gmail.com>
2022-10-25 20:00:28 +0300

    11.3 tasks all done

--- Promises, micro-macro tasks/11.3.js
+++ Promises, micro-macro tasks/11.3.js
@@ -5,3 +5,5 @@ promise.then(f1).catch(f2);
 // Versus:
 
 promise.then(f1, f2);
+
+//Answer: no, they not equal and don't have same way. IF f1 will have error, error will handle by catch. In second example don't have it
