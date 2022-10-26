ca783681506f280f01160470d573bb198682c97c
GedasIncirauskas <gedas.inc@gmail.com>
2022-10-26 19:13:43 +0300

    11.8 tasks all done

--- Promises, micro-macro tasks/11.8.js
+++ Promises, micro-macro tasks/11.8.js
@@ -1,13 +1,23 @@
 // Rewrite this example code from the chapter Promises chaining using async/await instead of .then/catch:
 
-function loadJson(url) {
-  return fetch(url).then((response) => {
-    if (response.status == 200) {
-      return response.json();
-    } else {
-      throw new Error(response.status);
-    }
-  });
+// function loadJson(url) {
+//   return fetch(url).then((response) => {
+//     if (response.status == 200) {
+//       return response.json();
+//     } else {
+//       throw new Error(response.status);
+//     }
+//   });
+// }
+
+async function loadJson(url) {
+  let response = await fetch(url);
+
+  if (response.status == 200) {
+    let json = await response.json();
+    return json;
+  }
+  throw new Error(response.status);
 }
 
 loadJson("https://javascript.info/no-such-user.json").catch(alert); // Error: 404
@@ -56,12 +66,53 @@ function demoGithubUser() {
 
 demoGithubUser();
 
+//ANSWER:
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = "HttpError";
+    this.response = response;
+  }
+}
+
+async function loadJson(url) {
+  let response = await fetch(url);
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new HttpError(response);
+  }
+}
+
+async function demoGithubUser() {
+  let user;
+  while (true) {
+    let name = prompt("Enter a name?", "iliakan");
+
+    try {
+      user = await loadJson(`https://api.github.com/users/${name}`);
+      break;
+    } catch (err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert("No such user, please reenter.");
+      } else {
+        throw err;
+      }
+    }
+  }
+
+  alert(`Full name: ${user.name}.`);
+  return user;
+}
+
+demoGithubUser();
+
 // _________________________________________________________________________________________
 
 // We have a “regular” function called f. How can you call the async function wait() and use its result inside of f?
 
 async function wait() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // function with async return promise
 
   return 10;
 }
@@ -70,5 +121,8 @@ function f() {
   // ...what should you write here?
   // we need to call async wait() and wait to get 10
   // remember, we can't use "await"
+  wait().then((result) => console.log(result));
 }
+
+f(); // ANSWER: return 10
 // P.S. The task is technically very simple, but the question is quite common for developers new to async/await.
